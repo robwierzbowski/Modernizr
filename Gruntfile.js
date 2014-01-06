bb4e7c6b85abe2f500c6e9da61fe171a7739f0ef
Rob Wierzbowski <hello@robwierzbowski.com>
2014-01-05 20:15:49 -0500

    Move banners into a node require file

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,27 +11,6 @@ module.exports = function( grunt ) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    banner: {
-      compact: '/*! <%= pkg.name %> <%= pkg.version %> (Custom Build) | <%= pkg.license %> */',
-      full: '/*!\n' +
-        ' * <%= pkg.name %> v<%= pkg.version %>\n' +
-        ' * modernizr.com\n *\n' +
-        ' * Copyright (c) <%= _.pluck(pkg.contributors, "name").join(", ") %>\n' +
-        ' * <%= pkg.license %> License\n */' +
-        ' \n' +
-        '/*\n' +
-        ' * Modernizr tests which native CSS3 and HTML5 features are available in the\n' +
-        ' * current UA and makes the results available to you in two ways: as properties on\n' +
-        ' * a global `Modernizr` object, and as classes on the `<html>` element. This\n' +
-        ' * information allows you to progressively enhance your pages with a granular level\n' +
-        ' * of control over the experience.\n' +
-        ' *\n' +
-        ' * Modernizr has an optional (*not included*) conditional resource loader called\n' +
-        ' * `Modernizr.load()`, based on [Yepnope.js](http://yepnopejs.com). You can get a\n' +
-        ' * build that includes `Modernizr.load()`, as well as choosing which feature tests\n' +
-        ' * to include on the [Download page](http://www.modernizr.com/download/).\n' +
-        ' */'
-    },
     meta: {
     },
     qunit: {
